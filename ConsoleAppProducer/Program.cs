4c14dbd7f3f4e9053b7419186be53181c74ec3ba
amar-tatwa <amarjyoti.mahanta@tatwa.info>
2024-11-11 16:18:38 +0530

    complete producer

---
+++ ConsoleAppProducer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppProducer;
+
+Console.WriteLine("Hello, World!");
+
+ProduceMessage produceMessage = new ProduceMessage();
+produceMessage.CreateMessage().Wait();
